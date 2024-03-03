d46d12345c29791d2ccfdb01b5cd203ed1b8c12e
hiroyuki <hiroyuki12@gmail.com>
2024-03-03 14:17:18 +0900

    First commit.

---
+++ maui_my_app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace maui_my_app;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ maui_my_app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace maui_my_app;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
